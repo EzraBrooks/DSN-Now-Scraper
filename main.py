41c82cdafbea94a0d896e13fb8048f1867e528a0
Ezra Brooks <ezra.m.brooks@gmail.com>
2017-08-23 00:28:06 -0400

    Make the scraper a custom thread

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
 import urllib.request
 import xmltodict
 import json
+import threading
+import time
 
-response = urllib.request.urlopen("https://eyes.nasa.gov/dsn/data/dsn.xml")
-print(json.dumps(xmltodict.parse(response.read().decode('utf-8')), indent=4))
+class ScraperThread(threading.Thread):
+    def __init__(self, method_to_call, timeout):
+        self.method_to_call = method_to_call
+        self.timeout = timeout
+        self.stopped = False
+        threading.Thread.__init__(self, daemon=True)
+    def run(self):
+        while not self.stopped:
+            self.method_to_call()
+            time.sleep(self.timeout)
+
+def get_as_dict():
+    response = urllib.request.urlopen("https://eyes.nasa.gov/dsn/data/dsn.xml")
+    return xmltodict.parse(response.read().decode('utf-8'))
+
+def main():
+    scraper_thread = ScraperThread(lambda: print(json.dumps(get_as_dict(), indent=4)), 1)
+    scraper_thread.start()
+    while True:
+        time.sleep(5)
+
+if __name__ == '__main__':
+    main()
